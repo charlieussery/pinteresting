c9ff19186c4dc531858e768ee07907e662f72756
charlieussery <charlie.ussery@gmail.com>
2015-02-01 00:54:39 -0800

    please god

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -11,7 +11,7 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = current_user.pins.new
+    @pin = current_user.pins.build
   end
 
  
@@ -20,7 +20,7 @@ class PinsController < ApplicationController
 
   
   def create
-    @pin = current_user.pins.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.' 
     else
