6bcef33ca1d1ac582615e8d241e827537fe1a677
charlieussery <charlie.ussery@gmail.com>
2015-02-01 20:56:54 -0800

    changed pins model to reflect default url

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,9 +2,12 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 
-	has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100" }
+	has_attached_file :image, 
+		styles: { medium: "300x300", thumb: "100x100" }, 
+		url: "http://charlie-pinteresting.s3.amazonaws.com/pins/images/:id_partition/:style/:basename.:extension" 
+
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 	validates :image, presence: true
 	validates :description, presence: true
-	
+
 end
